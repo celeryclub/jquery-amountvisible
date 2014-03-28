2c3f1e893e22a7e2229837f4a6627a70c8bef41c
Stephen Davis <stephendavis89@gmail.com>
2014-03-28 12:20:07 -0400

    Add live update on scroll to demo

--- demo/js/main.js
+++ demo/js/main.js
@@ -1,5 +1,14 @@
 $(function(){
+  $(window).scroll(updateSectionAmounts);
+
   $('section').on('click', function() {
     console.log($(this).amountVisible());
   });
 });
+
+var updateSectionAmounts = function() {
+  $('section').each(function() {
+    var $this = $(this);
+    $this.find('.amount').text($this.amountVisible());
+  });
+};
